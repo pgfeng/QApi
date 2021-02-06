b49bb0f540f0c1ecc14c68f00cb8ad8ac626877d
潘高峰 <228375325@qq.com>
2021-02-06 16:14:35 +0800

    BUG Fixed

--- src/Config.php
+++ src/Config.php
@@ -160,10 +160,10 @@ class Config
         if (!isset(self::$other[$config_name])) {
             $otherConfigPath = PROJECT_PATH . App::$configDir . DIRECTORY_SEPARATOR . $runMode
                 . DIRECTORY_SEPARATOR . $config_name . '.php';
-            if (file_exists($otherConfigPath)) {
+            if (!file_exists($otherConfigPath)) {
                 mkPathDir($otherConfigPath);
                 file_put_contents($otherConfigPath, file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'Config'
-                    . DIRECTORY_SEPARATOR . 'template' . DIRECTORY_SEPARATOR .$config_name. '.php'), LOCK_EX);
+                    . DIRECTORY_SEPARATOR . 'template' . DIRECTORY_SEPARATOR . 'config.php'), LOCK_EX);
             }
             self::$other[$config_name] = include $otherConfigPath;
         }
