50f08aee2f74059c7f8c3d6fa0f24e424f737065
潘高峰 <228375325@qq.com>
2021-05-14 11:55:03 +0800

    接口文档初版完成

---
+++ src/Attribute/Parameter/ParamsType.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace QApi\Attribute\Parameter;
+
+
+class ParamsType
+{
+    public const JSON = 'json';
+    public const ARRAY = 'array';
+    public const FLOAT = 'float';
+    public const INT = 'int';
+    public const STRING = 'string';
+    public const FILE = 'file';
+}
