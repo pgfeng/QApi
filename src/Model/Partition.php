5b2e702941e9e775ca7055766ae256b5238b8bf3
潘高峰 <228375325@qq.com>
2021-06-26 09:51:35 +0800

    BUG Fixed

--- src/Model/Partition.php
+++ src/Model/Partition.php
@@ -58,7 +58,8 @@ abstract class Partition extends Model
      */
     public function where(): self
     {
-        if (func_get_arg(0) === $this->partition_field) {
+        if (func_get_arg(0) === $this->partition_field || func_get_arg(0) === $this->getTable
+            (func_get_arg(1)) . '.' . $this->partition_field) {
             if (func_num_args() !== 2) {
                 throw new SqlErrorException('效验分表字段，where必须传入两个参数！');
             }
