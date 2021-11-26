4137eebbe0d19b110c5e0f055417586680435535
潘高峰 <228375325@qq.com>
2021-11-26 09:32:29 +0800

    Swoole Server Ok！

--- src/Command/RunSwooleCommand.php
+++ src/Command/RunSwooleCommand.php
@@ -52,6 +52,7 @@ class RunSwooleCommand extends CommandHandler
                 $response->header('Access-Control-Allow-Origin', $request->header['host']);
             }
             $response->header('Access-Control-Allow-Headers', implode(',', $appDomain['allowHeaders']));
+            $response->header('x-powered-by', 'QApi');
             $argv = [];
             $request->server['HTTP_HOST'] = $request->header['host'];
             $request->server = array_change_key_case($request->server, CASE_UPPER);
