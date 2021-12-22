6ee50cad2bab3a12a79991ca6cff042179446418
潘高峰 <228375325@qq.com>
2021-12-22 11:19:16 +0800

    Bug Fixed

--- src/Router.php
+++ src/Router.php
@@ -361,7 +361,7 @@ class Router
             } else {
                 $routers = array_keys($compileList[$method]);
                 foreach ($routers as $routerKey => $router) {
-                    if (preg_match('#^' . $router . '$#', $uri, $params)) {
+                    if (preg_match('#^' . $router . '$#i', $uri, $params)) {
                         $callback = $compileList[$method][$router];
                         array_shift($params);
                         break;
@@ -370,7 +370,7 @@ class Router
                 if (!$callback) {
                     $routers = array_keys($compileList['ALL']);
                     foreach ($routers as $routerKey => $router) {
-                        if (preg_match('#^' . $router . '$#', $uri, $params)) {
+                        if (preg_match('#^' . $router . '$#i', $uri, $params)) {
                             $callback = $compileList['ALL'][$router];
                             array_shift($params);
                             break;
