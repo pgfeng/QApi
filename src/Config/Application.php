3d706bb570ba1ddd6c7f4d3267b25d112d3795b4
潘高峰 <228375325@qq.com>
2022-02-18 10:04:52 +0800

    Response Raw

--- src/Config/Application.php
+++ src/Config/Application.php
@@ -37,7 +37,7 @@ class Application
                                 public string $defaultVersionName,
                                 public ?array $logHandler = null,
                                 public string|bool $nameSpace = false, public array $allowOrigin = ['*'],
-                                public array $allowHeaders = ['Request-Sign', 'Request-Time'],
+                                public array $allowHeaders = ['Request-Sign', 'Request-Time','Content-Type'],
                                 public string $applicationName = '', public string $scheme = 'http', public string $docPassword = '')
     {
         $this->appDir = trim($this->appDir, '/');
