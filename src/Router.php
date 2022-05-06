702dd13b2e123f1ae5890150a763869fe509b117
潘高峰 <228375325@qq.com>
2022-05-06 17:48:09 +0800

    deprecated Model.php

--- src/Router.php
+++ src/Router.php
@@ -209,6 +209,7 @@ class Router
         if (file_exists($lockFile)) {
             return;
         }
+        mkPathDir($lockFile);
         touch($lockFile);
         $builder_file_path = PROJECT_PATH . App::$routeDir . DIRECTORY_SEPARATOR . App::$app->getDir() .
             DIRECTORY_SEPARATOR
@@ -421,7 +422,7 @@ class Router
             } else {
                 $routers = array_keys($compileList[$method]);
                 foreach ($routers as $routerKey => $router) {
-                    if (preg_match('#^' . $router . '$#', preg_quote($uri,'#'), $params)) {
+                    if (preg_match('#^' . $router . '$#', preg_quote($uri, '#'), $params)) {
                         $callback = $compileList[$method][$router];
                         array_shift($params);
                         break;
@@ -430,7 +431,7 @@ class Router
                 if (!$callback) {
                     $routers = array_keys($compileList['ALL']);
                     foreach ($routers as $routerKey => $router) {
-                        if (preg_match('#^' . $router . '$#', preg_quote($uri,'#'), $params)) {
+                        if (preg_match('#^' . $router . '$#', preg_quote($uri, '#'), $params)) {
                             $callback = $compileList['ALL'][$router];
                             array_shift($params);
                             break;
