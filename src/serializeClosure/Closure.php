6b3cd27c18ddcb5f0d1969879457a923512f2926
PGF <service@9icode.net>
2022-05-18 14:05:02 +0800

    Fixed Bug.

--- src/serializeClosure/Closure.php
+++ src/serializeClosure/Closure.php
@@ -9,18 +9,28 @@ class Closure
 {
 
     public ?Closure $closure;
-
+    public ?array $use;
     private string $closureCode;
 
     public function __construct(\closure $closure)
     {
         $ref = new ReflectionFunction($closure);
-        $params = $ref->getParameters();
-        $paramData = [];
-        foreach ($params as $param) {
-            $paramData[] = '$' . $param->getName();
-        }
-        $this->closureCode = preg_replace('/function[\s]?\((.+)\)(.+){/', "function(" . implode(', ', $paramData) . "){", closureToStr($closure), 1);
+        $path = $ref->getFileName();  // absolute path of php file
+        $begn = $ref->getStartLine(); // have to `-1` for array index
+        $endn = $ref->getEndLine();
+        $dlim = "\n"; // or PHP_EOL
+        $content = file_get_contents($path);
+        $list = explode($dlim, $content);         // lines of php-file source
+        $list = array_slice($list, ($begn - 1), ($endn - ($begn - 1))); // lines of closure definition
+        $last = (count($list) - 1); // last line number
+        $list[0] = ('function' . explode('function', $list[0])[1]);
+        $list[$last] = (explode('}', $list[$last])[0] . '}');
+        $code = implode($dlim, $list);
+        preg_match_all('/use[\s+](.*);/iUs', $content, $matches);
+        $this->closureCode = base64_encode(json_encode([
+            'code' => $code,
+            'use' => implode("\n", $matches[0]),
+        ], JSON_UNESCAPED_UNICODE));
     }
 
 
--- src/serializeClosure/ClosureStream.php
+++ src/serializeClosure/ClosureStream.php
@@ -9,53 +9,56 @@ class ClosureStream
 {
     const STREAM_PROTO = 'closure';
 
-    protected static $isRegistered = false;
+    protected static bool $isRegistered = false;
 
-    protected $content;
+    protected string $content;
 
-    protected $length;
+    protected int $length;
 
-    protected $pointer = 0;
+    protected int $pointer = 0;
 
-    function stream_open($path, $mode, $options, &$opened_path)
+    function stream_open($path, $mode, $options, &$opened_path):bool
     {
-        $this->content = "<?php\nreturn " . substr($path, strlen(static::STREAM_PROTO . '://')) . ";";
+        $data = substr($path, strlen(static::STREAM_PROTO . '://'));
+        $json = base64_decode($data);
+        $data = json_decode($json, true);
+        $this->content = "<?php\n" . $data['use'] . "\nreturn " . $data['code'] . ";";
         $this->length = strlen($this->content);
         return true;
     }
 
-    public function stream_read($count)
+    public function stream_read($count):string
     {
         $value = substr($this->content, $this->pointer, $count);
         $this->pointer += $count;
         return $value;
     }
 
-    public function stream_eof()
+    public function stream_eof():bool
     {
         return $this->pointer >= $this->length;
     }
 
-    public function stream_set_option($option, $arg1, $arg2)
+    public function stream_set_option($option, $arg1, $arg2):bool
     {
         return false;
     }
 
-    public function stream_stat()
+    public function stream_stat():int
     {
         $stat = stat(__FILE__);
         $stat[7] = $stat['size'] = $this->length;
         return $stat;
     }
 
-    public function url_stat($path, $flags)
+    public function url_stat($path, $flags):int
     {
         $stat = stat(__FILE__);
         $stat[7] = $stat['size'] = $this->length;
         return $stat;
     }
-    
-    public function stream_seek($offset, $whence = SEEK_SET)
+
+    public function stream_seek($offset, $whence = SEEK_SET):bool
     {
         $crt = $this->pointer;
 
@@ -79,7 +82,7 @@ class ClosureStream
         return true;
     }
 
-    public function stream_tell()
+    public function stream_tell():int
     {
         return $this->pointer;
     }
