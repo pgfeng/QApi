781644d34fec5a3de296decc570f12d4dee49de6
PGF <228375325@qq.com>
2023-07-25 17:32:07 +0800

    Fixed Bug.

--- src/Config/Cache/Remote.php
+++ src/Config/Cache/Remote.php
@@ -5,11 +5,25 @@ namespace QApi\Config\Cache;
 use QApi\Cache\RemoteAdapter;
 use QApi\Config\Abstracts\Cache;
 
+/**
+ * Class Remote
+ * @package QApi\Config\Cache
+ */
 class Remote extends Cache
 {
     public string $driver = RemoteAdapter::class;
     public string $name = 'Remote';
 
+    /**
+     * Remote constructor.
+     * @param string $host
+     * @param int $port
+     * @param string $scheme
+     * @param string $username
+     * @param string $password
+     * @param string $configName
+     * @param string $path
+     */
     public function __construct(
         public string $host,
         public int    $port = 80,
