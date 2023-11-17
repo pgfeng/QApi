a436b9829dd1081755955fc6dca36004c9e93fa9
PGF <228375325@qq.com>
2023-11-17 18:33:24 +0800

    Fixed Bug.

--- src/Console.php
+++ src/Console.php
@@ -7,6 +7,7 @@ use QApi\Console\make\ColumnCommand;
 use QApi\Console\RunCommand;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 use function Co\run;
 
 class Console
@@ -17,10 +18,11 @@ class Console
     'runtime')
     {
         $this->application = new Application('QApi Console Tool');
+        $this->application->getDefinition()->addOption(new InputOption('mode', 'm', InputOption::VALUE_OPTIONAL, '[dev|test|prod|debug|...]Runtime environment mode.', null));
         error_reporting(E_ALL ^ E_NOTICE);
         App::$timezone = new \DateTimeZone($timezone);
         date_default_timezone_set($timezone);
-        Logger::init('QApi-CLI');
+        Logger::init('QApiConsole');
         App::$routeDir = trim($routeDir, '/');
         App::$runtimeDir = trim($runtimeDir, '/');
         App::$configDir = trim($configDir, '/');
