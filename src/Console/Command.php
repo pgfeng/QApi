419216bfbd2e45eb93fbb9aedac7770d4b47c8fc
PGF <228375325@qq.com>
2024-06-23 23:12:55 +0800

    Fixed Bug.

--- src/Console/Command.php
+++ src/Console/Command.php
@@ -5,5 +5,7 @@ namespace QApi\Console;
 
 class Command extends \Symfony\Component\Console\Command\Command
 {
-
+    public const SUCCESS = 0;
+    public const FAILURE = 1;
+    public const INVALID = 2;
 }
