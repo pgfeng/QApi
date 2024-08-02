e0adb18e82e42f7f5edab5f4772439eb415fdb0a
PGF <228375325@qq.com>
2024-08-02 19:58:55 +0800

    Fixed Bug.

--- src/Route/router.php
+++ src/Route/router.php
@@ -1,9 +1,11 @@
 <?php
 $_SERVER['PATH_INFO'] = $_GET['_router'] = preg_replace('/\?(.*)/', '', $_SERVER['REQUEST_URI']);
-if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
-    return false;
+if (mime_content_type($_SERVER["DOCUMENT_ROOT"] .$_SERVER["SCRIPT_NAME"])!='text/x-php' && file_exists($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
+    header("Access-Control-Allow-Origin: *");
+    echo file_get_contents($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"]);
+} else {
+    $_SERVER["SCRIPT_FILENAME"] = '/index.php';
+    $_SERVER["SCRIPT_NAME"] = '/index.php';
+    $_SERVER["PHP_SELF"] = '/index.php';
+    include $_SERVER['DOCUMENT_ROOT'] . "/index.php";
 }
-$_SERVER["SCRIPT_FILENAME"] = '/index.php';
-$_SERVER["SCRIPT_NAME"] = '/index.php';
-$_SERVER["PHP_SELF"] = '/index.php';
-include $_SERVER['DOCUMENT_ROOT'] . "/index.php";
